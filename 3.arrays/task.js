1efa7031389b5f8388e7c2c86fecd58d6e5b010d
Gusennn <gusen14049@gmail.com>
2023-08-10 17:22:19 +0300

    3.array task-1 add

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+
+    if ( arr1.length !== arr2.length ) {
+        return console.log('разные значения массива');
+    }
+
+    const compareState = arr1.every((thisElement, ind) => thisElement === arr2[ind]);
+    if ( compareState ) {
+        console.log(compareState);
+    } else  {
+        console.log(compareState + ', разные индексы, хотя и одинаковые значения');
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
 }
+
+compareArrays ([8, 7, 2], [8, 1, 2])
+
+
+
