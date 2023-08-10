9fc498781eabd73b55373e9cf4ffb2cf7f7ee0fc
Gusennn <gusen14049@gmail.com>
2023-08-10 18:22:40 +0300

    ups... repush

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,18 @@
-// function compareArrays(arr1, arr2) {
-//
-//     if ( arr1.length !== arr2.length ) {
-//         return console.log('разные значения массива');
-//     }
-//
-//     const compareState = arr1.every((thisElement, ind) => thisElement === arr2[ind]);
-//     if ( compareState ) {
-//         console.log(compareState);
-//     } else  {
-//         console.log(compareState + ', разные индексы, хотя и одинаковые значения');
-//     }
-// }
-//
-// compareArrays ([8, 7, 2], [8, 1, 2])
+function compareArrays(arr1, arr2) {
+
+    if ( arr1.length !== arr2.length ) {
+        return console.log('разные значения массива');
+    }
+
+    const compareState = arr1.every((thisElement, ind) => thisElement === arr2[ind]);
+    if ( compareState ) {
+        console.log(compareState);
+    } else  {
+        console.log(compareState + ', разные индексы, хотя и одинаковые значения');
+    }
+}
+
+compareArrays ([8, 7, 2], [8, 1, 2])
 
 
 
