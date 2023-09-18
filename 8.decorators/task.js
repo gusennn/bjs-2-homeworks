39a2e7a035452c6f57ad9416a920d2292a188e3a
Gusennn <gusen14049@gmail.com>
2023-09-18 15:23:50 +0300

    decorator upd

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,45 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    return function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash === hash);
+        if(objectInCache) {
+            console.log("Из кэша: " + objectInCache.result)
+            return "Из кэша: " + objectInCache.result;
+        }
+
+        let result = func(...args);
+        cache.push({hash, result});
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    function wrapper(...args) {
+        if(timeoutId === null) {
+            func(...args);
+            wrapper.count++;
+        }
+
+        if(timeoutId) {  //Повторил код лекции
+            clearTimeout(timeoutId);
+        }
+
+        timeoutId = setTimeout(() => {
+            timeoutId = true;
+            func(...args);
+            wrapper.count++;
+        }, delay);
+        wrapper.allCount++
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
 }
